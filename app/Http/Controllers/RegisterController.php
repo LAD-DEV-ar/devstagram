f21a3fe70ba697a7c089bc78720077380f01715c
LAD-DEV-ar <eliasaarroyo@gmail.com>
2026-05-25 17:18:02 -0300

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('auth.register');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required|confirmed|min:6',
+            'password_confirmation' => 'required'
+        ], [
+            'name.required' => 'El nombre es obligatorio',
+            'name.max' => 'Ese nombre es demasiado largo',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+    }
+}
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+Principal
+@endsection
+
+@section('contenido')
+Este es el contenido principal de la web
+@endsection
