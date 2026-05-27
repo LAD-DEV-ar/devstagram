6f85732adff8ddbd0e999751e01d4718b32194e0
LAD-DEV-ar <eliasaarroyo@gmail.com>
2026-05-27 14:39:05 -0300

    Agregue route al login en el navbar, en app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
             <h1 class="text-4xl font-extrabold">DevStagram</h1>
 
             <nav class="flex gap-2 items-center">
-                <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('login') }}">Login</a>
                 <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
             </nav>
         </div>
